24cb62156b0ed42df53c941d5adfc2d4eaf2eff1
Niko Kovacevic <nikovacevic@gmail.com>
2019-01-19 16:14:50 -0700

    Add header scrolling state; hide menu button on med+ screens; fix rgb colors

--- assets/js/divide100.js
+++ assets/js/divide100.js
@@ -3,34 +3,31 @@ $(function () {
   UI.viewportHeight = $(window).height();
   UI.viewportWidth = $(window).width();
   UI.floatingNav = $("nav.floating-nav");
+  UI.headerNav = $("nav.header-nav");
 
   UI.init = function () {
-    // Set ATF image
+    // Set ATF image height
     $("section.atf").css('height', UI.viewportHeight);
-
-    // Set fixed images
+    // Set fixed image heights
     $("section.fixed-image").css('height', UI.viewportHeight * 0.45);
-    setTimeout(function () {
-      $("body").removeClass('loading');
-    }, 250)
-
-    // Remove loading state
-    setTimeout(function () {
-      $("main, body").removeClass('loading');
-    }, 200)
   }
 
-  // Full-screen nav toggle
+  // Set scrolling state
+  $(window).scroll(function (e, ui) {
+    if ($(document).scrollTop() > 60) {
+      $('header').addClass('scrolling');
+    } else {
+      $('header').removeClass('scrolling');
+    }
+  });
+
+  // Toggle floating nav
   UI.toggleFloatingNav = function (nav) {
     nav.toggleClass('hidden');
   }
-  $('.toggle-menu').click(function (e, ui) {
+  $('.toggle-menu').click(function () {
     UI.toggleFloatingNav(UI.floatingNav);
   });
-  $('body').click(function (e, ui) {
-    console.log(e);
-    // UI.toggleFloatingNav(UI.floatingNav);
-  })
 });
 
 $(document).ready(function () {
